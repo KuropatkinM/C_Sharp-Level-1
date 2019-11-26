113d63a02c4e5e8a1b61e6c0ec710604f04c2366
KuropatkinM <KuropatkinM@gmail.com>
2019-11-26 22:39:20 +0300

    Домашнее задание к Уроку 1

---
+++ DistanceBetweemPoints/Program.cs
@@ -0,0 +1,39 @@
+﻿// Куропаткин Максим
+
+// Написать программу, которая подсчитывает расстояние между точками с координатами х1,у1 и х2,у2 
+// по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)). 
+// Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой)
+using System;
+
+namespace DistanceBetweemPoints
+{
+    class Program
+    {
+        static void Pause()
+        {
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Сейчас мы попробуем определить расстояние между \nдвумя точками на плоскости, зная только их координаты");
+            
+            Console.WriteLine("Пожалуйста введите координаты точки 1 по оси Х");
+            double x1= double.Parse(Console.ReadLine());
+            Console.WriteLine("Пожалуйста введите координаты точки 1 по оси Y");
+            double y1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Пожалуйста введите координаты точки 2 по оси Х");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Пожалуйста введите координаты точки 2 по оси Y");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(Math.Pow(x2-x1,2.0) + Math.Pow(y2-y1,2.0));
+
+            string pattern = "Расстояние между точками - {0:F2}";
+            Console.WriteLine(pattern,result);
+
+            Pause();
+        }
+    }
+}
---
+++ NameAdress/Program.cs
@@ -0,0 +1,56 @@
+﻿// Куропаткин Максим
+
+// а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+
+
+using System;
+
+namespace NameAdress
+{
+    enum personality 
+    {
+        Максим,
+        Куропаткин,
+        Пушкино
+    }
+
+    class Program
+    {
+        static void Pause()
+        {
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+            static void Print(string ms, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(ms);
+
+        }
+        static void Main(string[] args)
+        {
+            personality name = personality.Максим;
+            personality surname = personality.Куропаткин;
+            personality town = personality.Пушкино;
+
+            Console.WriteLine("Имя: " + name + ". Фамилия: " + surname + ". Город проживания: " + town);
+
+            // б) *сделать задание, только вывод организовать в центре экрана
+            Console.SetCursorPosition(30, 10);
+            Console.WriteLine("Имя: " + name + ". Фамилия: " + surname + ". Город проживания: " + town);
+
+
+            // в) **сделать задание б с использованием собственных методов (например, print(string ms, int x,int y).
+
+            Print("Имя: " + name + ". Фамилия: " + surname + ". Город проживания: " + town, 30, 20);
+
+
+            Pause();
+        }
+
+
+
+
+
+    }
+}
